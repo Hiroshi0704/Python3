f0b57f482e0702d1328cb9d7ff28ac3a9e2ffe9e
渡邉　博 <hiroshi@MacBook.local>
2019-04-10 10:49:20 +0900

    edit

--- MakeShiftByGenomeAlgorithmVer1.py
+++ MakeShiftByGenomeAlgorithmVer1.py
@@ -1,7 +1,7 @@
-############################################ import
+######################################################################################## import
 import random
 from decimal import *
-############################################ class
+######################################################################################## class
 
 class GenomShift:
 
@@ -19,19 +19,25 @@ class GenomShift:
 	# ...
 	# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] staff last
 
-	evaluation = None
-	### evaluation = integer 数値が低いほど高評価
+	evaluation = 0
+	### evaluation = integer型 数値が低いほど高評価
 	
 	def __init__(self, length_shift, evaluation):
 		self.length_shift = length_shift
 		self.evaluation = evaluation
 		# width_shiftへ変換
+		'''
 		width_shift = []
 		for i in range(len(self.length_shift[0])):
 			m = []
 			for l in self.length_shift:
 				m.append(l[i])
-			width_shift.append(m) 
+			m = [ l[i] for l in self.length_shift ]
+			width_shift.append(m)
+		'''
+		# 上記の内包表記
+		width_shift = [[ l[i] for l in self.length_shift ] for i in range(len(self.length_shift[0])) ]
+
 		self.width_shift = width_shift
 
 
@@ -49,29 +55,35 @@ class GenomShift:
 	def setLengthShift(self, length_shift):
 		self.length_shift = length_shift
 		# width_shiftへ変換
+		'''
 		width_shift = []
 		for i in range(len(self.length_shift[0])):
 			m = []
 			for l in self.length_shift:
 				m.append(l[i])
-			width_shift.append(m) 
+			width_shift.append(m)
+		'''
+		width_shift = [[ l[i] for l in self.length_shift ] for i in range(len(self.length_shift[0])) ]
 		self.width_shift = width_shift
 	
 	def setWidthShift(self, width_shift):
 		self.width_shift = width_shift
 		# length_shiftへ変換
+		'''
 		length_shift = []
 		for i in range(len(self.width_shift[0])):
 			m = []
 			for w in self.width_shift:
 				m.append(w[i])
 			length_shift.append(m)
+		'''
+		length_shift = [[ w[i] for w in self.width_shift ] for i in range(len(self.width_shift[0])) ]
 		self.length_shift = length_shift
 
 	def setEvaluation(self, evaluation):
 		self.evaluation = evaluation
 
-############################################ def
+######################################################################################## def
 ### 命名ルール ###
 # クラス複数 = objects
 # クラス単体 = obj
@@ -82,12 +94,12 @@ class GenomShift:
 #####
 
 # クラスインスタンスを生成する
-def create(shift_pattern, day_length):
+def create(weekday_shift_pattern, day_length):
 
 	length_shift = []
 	for d in range(day_length):
-		sp = shift_pattern
-		ls = [i for i in random.sample(sp, len(sp))]
+		wp = weekday_shift_pattern
+		ls = [i for i in random.sample(wp, len(wp))]
 		length_shift.append(ls)
 		
 	return GenomShift(length_shift, 0)
@@ -130,26 +142,69 @@ def evaluate(obj):
 	#### 上記整理 ####
 	# 7連続勤務 #
 	'X' = 休み
-	length_shiftを使用
+	width_shiftを使用
 	['A','C','A','A','C','A','C'.......] work_cnt = 7
 	['A','C','X','A','C','X','C'.......] work_cnt = 1
 
 	# 夜勤明けに日勤 #
-	夜勤明けに日勤の場合 = True
-	length_shiftを使用
+	'B' = 夜勤
+	夜勤明けに日勤の場合 = true_cnt += 1
+	width_shiftを使用
 	['B','X','B','A','B','C','X'.......] true_cnt = 2
 
 	# 1人１日希望休の申請 #
 	８人の場合、８人の希望休をリストへ格納
-	[1, 4, 6, 8, 9, 10, 26, 15] ３人目の希望休は６日目
-	width_lengthを使用
+	[1, 4, 6, 8, 9, 10, 26, 15] ３人目の希望休は６日目となる
+	length_shiftを使用
 	希望休ではない場合は休みの人と入れ替える
 
 	# 平均的な勤務時間 #
-	length_shiftを使用
-	勤務時間を計算し	
+	width_shiftを使用
+	勤務時間を計算し人数で割った平均と
+	個人の勤務時間
+	(+ -　５時間)以内を良しとする
 	'''
-	pass
+
+	obj.setEvaluation(0)
+	point = 0
+
+	### ７連続勤務判定 ### 
+	# 評価値：３
+	P = 3
+	# print('### ７連続勤務判定 ###')
+	for shift in obj.getWidthShift():
+		work_cnt = 0
+		for s in shift:
+			if work_cnt >= 7:
+				# print(shift)
+				work_cnt = 0
+				point += P
+				continue
+
+			if s == REST:
+				work_cnt = 0
+			else:
+				work_cnt += 1
+
+	### 夜勤明けに日勤（夜勤後は必ず休み） ###
+	# 評価値：２
+	P = 2
+	# print('### 夜勤明け日勤判定 ###')
+	for shift in obj.getWidthShift():
+		for i in range(len(shift)):
+			# 初日をスキップ
+			if i == 0: continue
+			# 昨晩夜勤だった場合　かつ　今日休み以外の場合
+			if shift[i-1] == NIGHT and shift[i] != REST:
+				# print('day: {}'.format(i+1))
+				# print(shift)
+				point += P
+			else: continue
+
+
+
+	obj.setEvaluation(point)
+	return obj
 	
 ### 命名ルール ###
 # クラス複数 = objects
@@ -167,7 +222,7 @@ def mutation(objects, individual_mutation, day_mutation, weekday_shift_pattern):
 	dm = day_mutation
 	wp = weekday_shift_pattern
 	
-	new_objects = []
+	mutant_objects = []
 	for obj in objects:
 		if im > (random.randint(0, 100) / Decimal(100)):
 			mutant_shift = []
@@ -177,25 +232,28 @@ def mutation(objects, individual_mutation, day_mutation, weekday_shift_pattern):
 					mutant_shift.append(shift)
 				else:
 					mutant_shift.append(shift)
-			new_objects.append(GenomShift(mutant_shift, 0))
+			mutant_objects.append(GenomShift(mutant_shift, 0))
 		else:
-			new_objects.append(obj)
-	return new_objects
+			mutant_objects.append(obj)
+	return mutant_objects
 
 # 新たな世代を作成
 def create_new_objects(objects, elite_objects, cross_objects):
-	new_objects = sorted(objects, reverse=True, key=lambda u:u.evaluation)
+	sorted_objects = sorted(objects, reverse=True, key=lambda u:u.evaluation)
 	
 	n = len(elite_objects) + len(cross_objects)
 	for i in range(n):
-		new_objects.pop(0)
-	new_objects.extend(elite_objects)
-	new_objects.extend(cross_objects)
-	return new_objects
+		sorted_objects.pop(0)
+	sorted_objects.extend(elite_objects)
+	sorted_objects.extend(cross_objects)
+	# print(sorted_objects)
+	return sorted_objects
 
 
-############################################ setting
-WEEKDAY_SHIFT_PATTERN = ['A','A','A','B','B','C','X','X']
+######################################################################################## setting
+WEEKDAY_SHIFT_PATTERN = ['A','A','A','B','C','X','X','X']
+REST = 'X'
+NIGHT = 'B'
 
 DAY_LENGTH = 28
 ELITE_LENGTH = 20
@@ -205,7 +263,7 @@ MAX_GENERATION = 40
 
 INDIVIDUAL_MUTATION = 0.1
 DAY_MUTATION = 0.1
-############################################ main
+######################################################################################## main
 
 ### 命名ルール ###
 # クラス複数 = objects
@@ -221,51 +279,51 @@ if __name__=='__main__':
 	elite_objects = []
 	cross_objects = []
 	
-	for i in range(MAX_SHIFT_LENDTH):
-		m = create(WEEKDAY_SHIFT_PATTERN, DAY_LENGTH)
-		objects.append(m)
-	
-	print('\n========== object[0] ==========')
-	for i in objects[0].getWidthShift():
-		print(i)
+	# 初期状態
+	objects = [ create(WEEKDAY_SHIFT_PATTERN, DAY_LENGTH) for i in range(MAX_SHIFT_LENDTH) ]
+	# print(objects)
+
+	# 世代交代開始
+	for count in range(1, MAX_GENERATION + 1):
+		# 評価
+		m = []
+		for obj in objects:
+			m.append(evaluate(obj))
+		objects = m
+		# エリート選択
+		elite_objects = select(objects, ELITE_LENGTH)
+		# 交叉
+		cross_objects = []
+		for i in range(ELITE_LENGTH):
+			m = crossover(elite_objects[i-1], elite_objects[i], DAY_LENGTH)
+			cross_objects.extend(m)
+		# cross_objects = [ crossover(elite_objects[i-1], elite_objects[i], DAY_LENGTH) for i in range(ELITE_LENGTH)]
+		# print(cross_objects[0].getLengthShift())
+
 		
-	elite_objects = select(objects, ELITE_LENGTH)
-	
-	print('\n========== elite[0] ==========')
+		# 世代交代
+		new_objects = create_new_objects(objects, elite_objects, cross_objects)
+		
+		# 変異
+		new_objects = mutation(new_objects, INDIVIDUAL_MUTATION, DAY_MUTATION, WEEKDAY_SHIFT_PATTERN)
+
+		# 進化結果を表示
+		fits = [i.getEvaluation() for i in objects]
+
+		min_ = min(fits)
+		max_ = max(fits)
+		avg_ = sum(fits) / Decimal(len(fits))
+
+		print ("-----第{}世代の結果-----".format(count))
+		print ("  Min:{}".format(min_))
+		print ("  Max:{}".format(max_))
+		print ("  Avg:{}".format(avg_))
+
+        # 現行と入れ替え
+		objects = new_objects
+
+	print('=== 最適化したシフト ===')
 	for i in elite_objects[0].getWidthShift():
 		print(i)
-	
-	for i in range(len(elite_objects)):
-		m = crossover(elite_objects[i-1], elite_objects[i], DAY_LENGTH)
-		cross_objects.extend(m)
-	
-	print('\n========== cross[0] ==========')
-	for i in cross_objects[0].getWidthShift():
-		print(i)
-		
-	objects = mutation(objects, INDIVIDUAL_MUTATION, DAY_MUTATION, WEEKDAY_SHIFT_PATTERN)
-	
-	print('\n========== mutant[0] ==========')
-	for i in objects[0].getWidthShift():
-		print(i)
-	
-	new_objects = create_new_objects(objects, elite_objects, cross_objects)
-	
 
-############################################ test
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+######################################################################################## test
