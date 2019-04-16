ed185959f9f875e3e3ce2bc1e2ce1fb44673d2f6
渡邉　博 <hiroshi@MacBook.local>
2019-04-16 13:12:12 +0900

    ver 1.2.3

--- MakeShiftByGenomeAlgorithm.py
+++ MakeShiftByGenomeAlgorithm.py
@@ -303,7 +303,7 @@ def evaluate_work_time(obj, work_time):
         # １人の勤務時間が平均的かどうか判定
         if int(avg_ + min_max) <= cwt or int(avg_ - min_max) >= cwt:
             # point += P
-            # 規定の範囲外なら、(絶対値(超えた時間 - 平均時間)　/ 100)
+            # 規定の範囲外なら、(絶対値(勤務時間 - 平均時間)　/ 100)
             point += Decimal(abs(cwt - avg_)) / Decimal(100)
             
     m = obj.getEvaluation()
